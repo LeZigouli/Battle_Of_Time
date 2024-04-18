7f949a02b6ad6fe6be8b261bb7e9c2a457800f4b
VincentPRD <vincent.proudy.etu@univ-lemans.fr>
2024-04-18 13:59:03 +0200

    fix sprite

--- Conception/bot/lib/attaque.h
+++ Conception/bot/lib/attaque.h
@@ -5,6 +5,6 @@
 
 void resize_dep(SDL_Rect * atimg,SDL_Rect * position, character_t * c);
 void resize_att(SDL_Rect * atimg,SDL_Rect * position, character_t * c);
-void ataquage(SDL_Rect * atimg,SDL_Rect * position, character_t * c, int * attaque);
+void ataquage(SDL_Rect * atimg,SDL_Rect * position, character_t * c, int * attaque, unsigned long * fin, unsigned long * debut);
 
 #endif
